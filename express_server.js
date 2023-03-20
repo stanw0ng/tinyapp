5d01fd150ccc1d1f1f8a3788ed61a034251af1e2
stanw0ng <user.stanley@gmail.com>
2023-03-20 18:10:53 -0400

    added post route to recieve user input at /urls/new

--- express_server.js
+++ express_server.js
@@ -27,8 +27,14 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+//must go before /urls/:id
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:id", (req, res) => {
